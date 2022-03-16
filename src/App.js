98dfcc1277171ab326561b30755216bd9a9e8130
Paul Caruana <paulmcaruana@outlook.com>
2022-03-16 14:53:17 +0000

    Web3 added and utilised in browser and not MetaMask version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import React, { Component } from 'react';
 
-function App() {
+class App extends Component { 
+  render() {
+    web3.eth.getAccounts().then(console.log);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hi There!</h1>
     </div>
   );
 }
+ }
+
 
 export default App;
